37ff8636a311d1ca4165ebd9f6026b0fe7e9ec64
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2021-05-09 16:10:54 +0300

    commit "change MyPosts"

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 import {PostPropsType} from "../../../redux/state";
@@ -16,26 +16,25 @@ function MyPosts(props: PropsType) {
     let postsElements =
         props.posts.map(p => <Post key={p.id} id={p.id} message={p.message} likesCount={p.likesCount}/>)
 
-
-    let newPostElement = React.createRef<HTMLTextAreaElement>();
-
     const addPost = () => {
-        if (newPostElement.current) {
+        props.addPost(props.newPostText)
+       /* if (newPostElement.current) {
             props.addPost(newPostElement.current.value)
-        }
+        }*/
     }
 
-    const onPostChange = () => {
-        if (newPostElement.current) {
+    const onPostChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        props.updateNewPostText(e.currentTarget.value)
+        /*if (newPostElement.current) {
             props.updateNewPostText(newPostElement.current.value);
-        }
+        }*/
     }
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <div><textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/></div>
+                <div><textarea onChange={onPostChange} value={props.newPostText}/></div>
                 <div>
                     <button onClick={addPost}>Add post</button>
                 </div>
