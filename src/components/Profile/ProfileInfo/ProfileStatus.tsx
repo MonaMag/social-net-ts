27bb73d232179bee2d8cbf05c99e7a2df9823a4a
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2021-08-21 16:39:45 +0300

    L74  used componentDidMount

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -32,6 +32,14 @@ export class ProfileStatus extends React.Component<PropsType, StateType> {
         })
     }
 
+    componentDidUpdate(prevProps: Readonly<PropsType>, prevState: Readonly<StateType>) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
 
     render() {
         return (
